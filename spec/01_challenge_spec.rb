e080b5f5ec35a4e991980ed2c3bf7c02934a945c
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 01:43:45 +0000

    Automatically backed up by Learn

--- spec/01_challenge_spec.rb
+++ spec/01_challenge_spec.rb
@@ -4,6 +4,6 @@ require_relative '../lib/first_challenge.rb'
 describe "first_challenge" do
 	it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes" do
     expect(first_challenge.keys).to eq([:montague, :capulet])
-    expect(first_challenge.values).to eq([{}, {}])
+    expect(first_challenge.values).to eq([{"dead"}, {"alive"}])
   end
 end
